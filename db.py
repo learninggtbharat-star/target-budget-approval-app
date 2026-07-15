29c39fe691f8ede1b58b4bc6357a086e07ffceb6
poorviarora05 <149042160+poorviarora05@users.noreply.github.com>
2026-07-16 00:53:45 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,10 +1,12 @@
+import streamlit as st
 import mysql.connector
+
 
 def get_connection():
     return mysql.connector.connect(
-        host="tokaido.proxy.rlwy.net",
-        port=36470,
-        user="root",
-        password="EkEmFVwYTMPjKHBqOGbvHlrGROWhpMDo",
-        database="railway"
+        host=st.secrets["mysql"]["host"],
+        port=int(st.secrets["mysql"]["port"]),
+        user=st.secrets["mysql"]["user"],
+        password=st.secrets["mysql"]["password"],
+        database=st.secrets["mysql"]["database"]
     )
